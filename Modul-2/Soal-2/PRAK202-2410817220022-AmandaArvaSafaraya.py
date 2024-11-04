fe2158008fffbbc9009d97490a5b01b9bc122500
Amanda-06 <mnd.06.as@gmail.com>
2024-11-04 13:33:34 +0700

    Delete Modul-2 directory

--- Modul-2/Soal-2/PRAK202-2410817220022-AmandaArvaSafaraya.py
+++
@@ -1,4 +0,0 @@
-nilaiPertama = float(input("Masukkan Nilai Pertama : "))
-nilaiKedua = float(input("Masukkan Nilai Kedua : "))
-hasil = nilaiPertama + nilaiKedua
-print(f'Hasil dari penjumlahan nilai pertama "{nilaiPertama:g}" dan nilai kedua "{nilaiKedua:g}" adalah "{hasil:.2f}"')
