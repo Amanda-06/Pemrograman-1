8820a51bfc284103fa4698f33fed5630b30c684e
Amanda-06 <mnd.06.as@gmail.com>
2024-12-12 12:59:00 +0800

    Add files via upload

---
+++ Modul-5/Soal-5/PRAK505-2410817220022-AmandaArvaSafaraya.py
@@ -0,0 +1,13 @@
+def Biodata(tahunLahir, Namaku, Asal) :
+    tahun_sekarang = 2020
+    umur = tahun_sekarang - tahunLahir
+    print("Perkenalkan Nama Saya : ", Namaku)
+    print("Umur Saya : ", umur)
+    print("Saya Adalah Angkatan : ", tahun_sekarang)
+    print("Asal Saya dari : ", Asal)
+
+tahunLahir = int(input())
+Namaku = input()
+Asal = input()
+print()
+Biodata(tahunLahir, Namaku, Asal)
